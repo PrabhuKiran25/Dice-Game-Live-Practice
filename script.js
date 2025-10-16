4f212e5c0412f9b11d96119e01925520e0697282
PrabhuKiran25 <prabhuakula06@gmail.com>
2025-10-16 15:57:43 +0530

    Start and Update Moves Completed

--- script.js
+++ script.js
@@ -9,7 +9,27 @@ const player2Button = document.getElementById("player2-button");
 
 // Start Function
 
+const handleStart = () => {
+  const playerStart = Math.floor(Math.random() * 2 + 1);
+  //console.log(playerStart);
+  updateMoves(playerStart === 1 ? "Player-1" : "Player-2");
+};
+
 // Update Moves Function
+
+const updateMoves = (player) => {
+  // Update the player move value
+  playerMove.innerText = `${player} To Play`;
+
+  // Disabling the button based on the player
+  if (player === "Player-1") {
+    player1Button.disabled = false;
+    player2Button.disabled = true;
+  } else {
+    player1Button.disabled = true;
+    player2Button.disabled = false;
+  }
+};
 
 // Dice Roll Logic
 
